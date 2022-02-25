135392530162f217e95f8fe48725f0cacae6b212
allpha23 <allpha23@gmail.com>
2022-02-25 01:37:20 -0300

    Requisito 4 finalizado

---
+++ desafio4.sql
@@ -0,0 +1,8 @@
+SELECT DISTINCT (c.customer) AS usuario,
+  IF(MAX(YEAR(h.play_date)) = '2021', 'Usuario ativo', 'Usuario inativo') AS 'condicao_usuario'
+FROM 
+  SpotifyClone.customer AS c
+  INNER JOIN
+    SpotifyClone.history AS h
+    ON c.customer_id = h.customer_id
+GROUP BY h.customer_id ORDER BY c.customer;
