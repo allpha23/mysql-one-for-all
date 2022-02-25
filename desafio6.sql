f1a567561eed0fb3a468e4b4e1b93a9cce8f81da
allpha23 <allpha23@gmail.com>
2022-02-25 04:15:19 -0300

    Requisito 6 correção

--- desafio6.sql
+++ desafio6.sql
@@ -1,5 +1,5 @@
 SELECT 
-  ROUND(MIN(p.value_plan), 2) AS faturamento_minimo,
+  FORMAT(MIN(p.value_plan), 2) AS faturamento_minimo,
   ROUND(MAX(p.value_plan), 2) AS faturamento_maximo,
   ROUND(AVG(p.value_plan), 2) AS faturamento_medio,
   ROUND(SUM(p.value_plan), 2) AS faturamento_total
